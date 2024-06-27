35bc3f5f46d984895ed15a23ddf8a1d92dd3d172
Sahil Sandhu <21f1002317@ds.study.iitm.ac.in>
2024-06-27 22:09:05 +0530

    honor code
    Added honor code 21F1002317

---
+++ Frontend/se_project/src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-light bg-white fixed">
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item active">
+                        <router-link to="#" class="nav-link" aria-current="page">Home</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+
+export default{
+    name: 'HeaderBar',
+}
+
+</script>
