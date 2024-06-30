2708ca0499f60b9a756ea17ebd12b279d4e39837
Sahil Sandhu <21f1002317@ds.study.iitm.ac.in>
2024-06-30 14:30:07 +0530

    Restructure directory
    Move scripts to milestone 6 directory.

---
+++ Milestone 6/Code/Frontend/se_project/src/components/landing_page.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <HeaderBar></HeaderBar>
+        <h1>Landing Page.</h1>
+    </div>
+</template>
+
+<script>
+
+import HeaderBar from './header.vue';
+
+export default{
+    name: 'LandingPage',
+    components: {
+        HeaderBar
+    }
+}
+</script>
